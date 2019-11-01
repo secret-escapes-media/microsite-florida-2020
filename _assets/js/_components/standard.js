e8cf25f800b1769c399cff66a2f08cc4db198ded
Birgit <birgit.voslamber@secretescapes.com>
2019-11-01 09:25:54 +0100

    copied florida 2019

---
+++ _assets/js/_components/standard.js
@@ -0,0 +1,80 @@
+///////////////////////////////////////
+//      smooth-scrolling - http://css-tricks.com/snippets/jquery/smooth-scrolling/
+///////////////////////////////////////
+$(function() {
+  $('a[href*=\\#]:not([href=\\#])').click(function() {
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
+
+
+///////////////////////////////////////
+//      inserts current year
+///////////////////////////////////////
+$('.js-year').html(new Date().getFullYear());
+
+
+///////////////////////////////////////
+//      detects touch device
+///////////////////////////////////////
+if ("ontouchstart" in document.documentElement){
+  $('html').addClass('touch');
+} else {
+  $('html').addClass('no-touch');
+}
+
+
+///////////////////////////////////////
+//        Navigation
+///////////////////////////////////////
+
+// mobile nav toggle open & close
+$('.js-toggle-mobile-nav').on('click', function(e) {
+  $('.mobile-nav').toggleClass('is-open').toggleClass('is-closed');
+});
+
+
+///////////////////////////////////////
+//        GET QUERY STRING VALUE
+//-------------------------------------
+//        ?modal=video
+//        var queryValue = queryString('modal');
+//        queryValue = "video"
+///////////////////////////////////////
+
+
+function queryString(sParam){
+  var sPageURL = window.location.search.substring(1);
+  var sURLVariables = sPageURL.split("&");
+  for (var i = 0; i < sURLVariables.length; i++){
+    var sParameterName = sURLVariables[i].split("=");
+    if (sParameterName[0] == sParam){
+      return sParameterName[1];
+    }
+  }
+}
+
+
+///////////////////////////////////////
+//   query string searcher
+///////////////////////////////////////
+
+// searches for specific queryString, returns value or true if empty value
+function getQueryStringByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return true;
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
